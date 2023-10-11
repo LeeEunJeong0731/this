6275d4fe6e501fa5791f0a067879c6becb903a4d
이은정 <leeeunjeong@ieunjeong-ui-MacBookAir.local>
2023-10-11 10:54:09 +0900

    일반함수가 아닌 생성자 함수륾 만들어냈다

--- newfile.js
+++ newfile.js
@@ -2,5 +2,9 @@ function student(name, number) {
   this.name = name;
   this.number = number;
 }
-let test = new student("이은정", "0");
-console.log(test);
+let list = ["정영식", "신동현"];
+let studentArr = [];
+for (let i = 0; i < list.length; i++) {
+  studentArr[i] = new student(list[i], i + 1);
+}
+console.log(studentArr);
